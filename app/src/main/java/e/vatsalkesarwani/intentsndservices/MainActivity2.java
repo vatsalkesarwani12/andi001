09c6a8e9b2393d750cc475676f7624889dd486f9
iamvatsal <vatsalkesarwani12@gmail.com>
2019-10-05 22:31:17 +0530

    Updated my application .. Activity is modifies with a Court Counter App Court Counter App calculates the score among Two players added undo button in it and few design...

--- app/src/main/java/e/vatsalkesarwani/intentsndservices/MainActivity2.java
+++ app/src/main/java/e/vatsalkesarwani/intentsndservices/MainActivity2.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
 
     private Button button;
+    int aa=0,bb=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +38,135 @@ public class MainActivity2 extends AppCompatActivity {
 
     }
 
+    int fa=0,fb=0;
+    public void plus1a(View view)
+    {
+        aa+=1;
+        fa=1;
+        displaya();
+    }
+
+    public void plus2a(View view)
+    {
+        aa+=2;
+        fa=2;
+        displaya();
+    }
+
+    public void plus3a(View view)
+    {
+        aa+=3;
+        fa=3;
+        displaya();
+    }
+
+    public void plus1b(View view)
+    {
+        bb+=1;
+        fb=1;
+        displayb();
+    }
+
+    public void plus2b(View view)
+    {
+        bb+=2;
+        fb=2;
+        displayb();
+    }
+
+    public void plus3b(View view)
+    {
+        bb+=3;
+        fb=3;
+        displayb();
+    }
+
+    public void reset(View view)
+    {
+        aa=0;
+        bb=0;
+        displaya();
+        displayb();
+    }
+
+    public void displaya()
+    {
+        TextView teaa=(TextView)findViewById(R.id.a);
+        teaa.setText(""+aa);
+    }
+
+    public void displayb()
+    {
+        TextView teab=(TextView)findViewById(R.id.b);
+        teab.setText(""+bb);
+    }
+
+
+
     public void openWEB()
     {
         Intent intent=new Intent(this,MainActivity3.class);
         startActivity(intent);
+    }
+
+
+    public void undob(View view)
+    {
+        if(fb==0)
+            Toast.makeText(this,"Only One Undo Allowed",Toast.LENGTH_SHORT).show();
+        if(fb==1)
+        {
+            bb-=1;
+            fb=0;
+            if(bb<0)
+                bb=0;
+            displayb();
+        }
+        if(fb==2)
+        {
+            bb-=2;
+            fb=0;
+            if(bb<0)
+                bb=0;
+            displayb();
+        }
+        if(fb==3)
+        {
+            bb-=3;
+            fb=0;
+            if(bb<0)
+                bb=0;
+            displayb();
+        }
+    }
+
+    public void undoa(View view)
+    {
+        if(fa==0)
+            Toast.makeText(this,"Only One Undo Allowed",Toast.LENGTH_SHORT).show();
+        if(fa==1)
+        {
+            aa-=1;
+            fa=0;
+            if(aa<0)
+                aa=0;
+            displaya();
+        }
+        if(fa==2)
+        {
+            aa-=2;
+            fa=0;
+            if(aa<0)
+               aa=0;
+            displaya();
+        }
+        if(fa==3)
+        {
+            aa-=3;
+            fa=0;
+            if(aa<0)
+                aa=0;
+            displaya();
+        }
     }
 }
