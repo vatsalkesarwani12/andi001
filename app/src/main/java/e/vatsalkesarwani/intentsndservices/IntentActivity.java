79e96207ae572e56798bb97bd769730e64f97b6c
iamvatsal <vatsalkesarwani12@gmail.com>
2020-05-02 02:27:06 +0530

    Learnt System intents and implicit intents

--- app/src/main/java/e/vatsalkesarwani/intentsndservices/IntentActivity.java
+++ app/src/main/java/e/vatsalkesarwani/intentsndservices/IntentActivity.java
@@ -1,18 +1,23 @@
 package e.vatsalkesarwani.intentsndservices;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toolbar;
 
 public class IntentActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button c,u,cc;
+    Button c,u,cc,sms,sysInt;
+    ImageView imgView;
+    final int REQUEST_CODE=100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +29,14 @@ public class IntentActivity extends AppCompatActivity implements View.OnClickLis
         c=findViewById(R.id.button4);
         u=findViewById(R.id.button5);
         cc=findViewById(R.id.button6);
+        sms=findViewById(R.id.button7);
+        imgView=findViewById(R.id.imageView);
+        sysInt=findViewById(R.id.button8);
         c.setOnClickListener(this);
         u.setOnClickListener(this);
         cc.setOnClickListener(this);
+        sms.setOnClickListener(this);
+        sysInt.setOnClickListener(this);
     }
 
     @Override
@@ -35,10 +45,10 @@ public class IntentActivity extends AppCompatActivity implements View.OnClickLis
         switch (id)
         {
             case R.id.button4:
-                Intent i=new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+                Intent i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (i.resolveActivity(getPackageManager())!=null)
                 {
-                    startActivity(i);
+                    startActivityForResult(i,REQUEST_CODE);
                 }
                 break;
             case R.id.button5:
@@ -61,6 +71,29 @@ public class IntentActivity extends AppCompatActivity implements View.OnClickLis
                     startActivity(intent1 );
                 }
                 break;
+            case R.id.button7:
+                String message="This is a text message";
+                Intent intent2=new Intent(Intent.ACTION_SENDTO);
+                intent2.setData(Uri.parse("sms:+99473704638"));
+                intent2.putExtra("sms_body",message);
+                if (intent2.resolveActivity(getPackageManager())!=null)
+                {
+                    startActivity(intent2);
+                }
+                break;
+            case R.id.button8:
+                startActivity(new Intent(getApplicationContext(),SystemIntent.class));
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==REQUEST_CODE && resultCode==RESULT_OK)
+        {
+            Bundle extras=data.getExtras();
+            Bitmap bitmapImage = (Bitmap) extras.get("data");
+            imgView.setImageBitmap(bitmapImage);
         }
     }
 }
---
+++ app/src/main/java/e/vatsalkesarwani/intentsndservices/SystemIntent.java
@@ -0,0 +1,97 @@
+package e.vatsalkesarwani.intentsndservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+public class SystemIntent extends AppCompatActivity implements View.OnClickListener {
+
+    Button call,map,alarm,email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_intent);
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+
+        alarm=findViewById(R.id.button9);
+        map=findViewById(R.id.button10);
+        email=findViewById(R.id.button11);
+        call=findViewById(R.id.button12);
+
+        alarm.setOnClickListener(this);
+        map.setOnClickListener(this);
+        email.setOnClickListener(this);
+        call.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id=v.getId();
+        switch (id)
+        {
+            case R.id.button9:
+                String mes="Time to wake up";
+                final int hour=2;
+                final int min=1;
+                Intent intent3=new Intent(AlarmClock.ACTION_SET_ALARM);
+                intent3.putExtra(AlarmClock.EXTRA_MESSAGE,mes);
+                intent3.putExtra(AlarmClock.EXTRA_HOUR,hour);
+                intent3.putExtra(AlarmClock.EXTRA_MINUTES,min);
+                intent3.putExtra(AlarmClock.EXTRA_VIBRATE,true);
+                if (intent3.resolveActivity(getPackageManager())!=null){
+                    startActivity(intent3);
+                }
+                break;
+            case R.id.button10:
+                String location="37.4220,-122.0481";//display coordinated location
+                // location="0,?q=37.4220,-122.0841(Google Plex)"    display the given coordinate with Google plex
+                // location="0,0?q=20+W+34th+St+10001"       display the given address
+                // location="47.205,-122.3493?q=restaurants"   find nearby restaurants of the given coordinates
+                Uri geoloc=Uri.parse("geo:"+location);
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(geoloc);
+                if (intent.resolveActivity(getPackageManager())!=null){
+                    startActivity(intent);
+                }
+                break;
+
+            case R.id.button11:
+                String mailmssg="This is the message";
+                String add[]={"test@gmail.com"};
+                String ccs[]={"user@gmail.com"};
+                String topic="Topic";
+                String subject="subject";
+                Intent intent5=new Intent(Intent.ACTION_SENDTO);
+                Uri emailuri=Uri.parse("mailto:");
+                intent5.setData(emailuri);
+                intent5.putExtra(Intent.EXTRA_SUBJECT,subject);
+                intent5.putExtra(Intent.EXTRA_EMAIL,add);
+                intent5.putExtra(Intent.EXTRA_CC,ccs);
+                intent5.putExtra(Intent.EXTRA_TEXT,mailmssg);
+                if (intent5.resolveActivity(getPackageManager())!=null)
+                {
+                    startActivity(intent5);
+                }
+                break;
+
+            case R.id.button12:
+                Intent intent4=new Intent(Intent.ACTION_CALL);
+                Uri urinum=Uri.parse("tel:"+"9473704638");
+                intent4.setData(urinum);
+                if (intent4.resolveActivity(getPackageManager())!=null)
+                {
+                    startActivity(intent4);
+                }
+                break;
+        }
+    }
+}
