bc974768d2d1ca71e91a84001b84aa7e744400b6
iamvatsal <vatsalkesarwani12@gmail.com>
2020-05-16 03:51:00 +0530

    Custom dialog made

--- app/src/main/java/e/vatsalkesarwani/intentsndservices/Activity/Dailogs.java
+++ app/src/main/java/e/vatsalkesarwani/intentsndservices/Activity/Dailogs.java
@@ -3,10 +3,12 @@ package e.vatsalkesarwani.intentsndservices.Activity;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import e.vatsalkesarwani.intentsndservices.R;
@@ -36,9 +38,26 @@ public class Dailogs extends AppCompatActivity implements View.OnClickListener {
                 break;
 
             case R.id.button15:
-
+                customDialog();
                 break;
         }
+    }
+
+    private void customDialog() {
+        final Dialog dialog=new Dialog(this);
+        dialog.setContentView(R.layout.dialog_custom);
+        dialog.setCancelable(false);
+        Button b=dialog.findViewById(R.id.customButton);
+        TextView t=dialog.findViewById(R.id.customText);
+        t.setText("This is a custom dialog box");
+        dialog.show();
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Dailogs.this, "custom dialog back button pressed", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
     }
 
     private void smplDailog() {
